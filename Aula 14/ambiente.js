b35261af3145f603bc3978305e7d280ad850690d
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2023-11-29 20:33:47 +0000

    Adiciona a aula 14

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+
+while (c <= 6) {
+    console.log(`Eu sou ${c}`)
+}
+*/
+
+//----------------------------
+console.log('Vai começar...')
+for (c=1; c<=6; c++) {
+    console.log(`Eu sou ${c}`)
+}
+console.log('FIM!')
